076991ad26f9926a4dd4f643eade2a2360035118
Jamam Witwit <jamamwitwit@MacBook-Pro-van-Jamam.local>
2017-03-21 14:51:47 +0100

    Animation occurring in the splash screen and waiting time increased

--- Android/BirdEncyclopedia/app/src/main/java/com/example/jamamwitwit/birdencylopedia/Splashscreen.java
+++ Android/BirdEncyclopedia/app/src/main/java/com/example/jamamwitwit/birdencylopedia/Splashscreen.java
@@ -47,7 +47,7 @@ public class Splashscreen extends Activity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 3500) {
+                    while (waited < 6000) {
                         sleep(100);
                         waited += 100;
                     }
