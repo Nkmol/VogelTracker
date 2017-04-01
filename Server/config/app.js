a2b5abcc0a23ee8d226845a4b8ebb18df6c41026
Nkmol <Sandermol95@hotmail.com>
2017-04-01 16:15:09 +0200

    Fix API CROSS headers

--- Server/config/app.js
+++ Server/config/app.js
@@ -17,6 +17,15 @@ module.exports.start = () => {
             // Setup listening
             let app = express();
 
+            // Add headers
+            app.use((req, res, next) => {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+                res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+                next();
+            })
+
             // Setup express middleware
             let bodyParser = require('body-parser') 
             app.use(bodyParser.json());       // to support JSON-encoded bodies
