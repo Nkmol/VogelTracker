3a7145ee1b13da49c0128ed2d0df8d772eef648b
Nkmol <Sandermol95@hotmail.com>
2017-04-04 17:05:08 +0200

    clean files

--- Client/app/src/common/content/content.component.js
+++ Client/app/src/common/content/content.component.js
@@ -5,37 +5,6 @@ const MenuComponent = {
     controller: class MenuComponent {
         constructor($localStorage) {
             'ngInject';
-            this.token = $localStorage.token;
-            this.markers = {
-                london: {
-                lat: 51.50,
-                lng: -0.082,
-                icon: {
-                    iconSize: [80, 80],
-                    iconAnchor: [40, 80],
-                    popupAnchor: [0, 0],
-                    shadowSize: [0, 0],
-                    shadowAnchor: [0, 0]
-                }
-                },
-                paris: {
-                    message: `
-                        <h1> Title of the balloon </h1>
-                        <span>Sub title</span>
-                        <a href="/#/register" ui-sref="register"> To register </a>
-                        <img src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg" />
-                    `,
-                    lat: 48.83,
-                    lng: 2.37,
-                    icon: {
-                        iconSize: [80, 80],
-                        iconAnchor: [40, 60],
-                        popupAnchor: [0, 0],
-                        shadowSize: [0, 0],
-                        shadowAnchor: [0, 0]
-                    }
-                }
-            }
         }
 
         $onInit() {
