7e14018a979ad90beb04c881d7bdaaab576b9046
Jamam Witwit <jamamwitwit@MacBook-Pro-van-Jamam.local>
2017-04-06 22:51:37 +0200

    Add more data

--- Server/doc/api_data.js
+++ Server/doc/api_data.js
@@ -497,6 +497,102 @@ define({ "api": [
     "groupTitle": "User"
   },
   {
+    "type": "get",
+    "url": "/reports/:id",
+    "title": "Request Report",
+    "name": "GetReport",
+    "group": "Report",
+    "parameter": {
+      "fields": {
+        "body": [
+          {
+            "group": "body",
+            "type": "ObjectId",
+            "optional": false,
+            "field": "id",
+            "description": "<p>bird ID <code>match(/^[0-9a-fA-F]{24}$/)</code></p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "type": "text",
+            "optional": false,
+            "field": "400/BadRequest",
+            "description": "<p>Please provide a valid '_id'</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "type": "text",
+            "optional": false,
+            "field": "401/Unauthorized",
+            "description": "<p>You have not provided your Token credentials as header request</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "modules/birds/router.js",
+    "groupTitle": "Report",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "optional": false,
+            "field": "Authorization",
+            "description": "<p>The JWT-token header: &quot;JWT {{ TOKEN }}&quot;.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "-",
+            "description": "<p>array of Birds</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "-.name",
+            "description": "<p>name of the Bird.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "-.latinname",
+            "description": "<p>latin_name of the Bird.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "-.information",
+            "description": "<p>some information about the bird</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "-.trend_and_amount",
+            "description": "<p>trends and history about the bird.</p>"
+          }
+        ]
+      }
+    }
+  },
+  {
     "type": "delete",
     "url": "/reports/",
     "title": "Delete Report",
