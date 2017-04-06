776abc538bd98dec689bc71988d36bf05044cedd
Nkmol <Sandermol95@hotmail.com>
2017-04-06 23:00:39 +0200

    Revert map changes

--- Client/app/src/index.js
+++ Client/app/src/index.js
@@ -1,5 +1,4 @@
 import App from './app.module.js';
-import './js'
 
 // NOTE: This will work if you have your scripts loaded at the end of the page (instead of in the header).
 //   http://stackoverflow.com/questions/16537783/which-method-should-i-use-to-manually-bootstrap-my-angularjs
