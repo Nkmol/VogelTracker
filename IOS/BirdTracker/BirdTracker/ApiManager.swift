41739b842da0fba751a4730aae9c3e89b16dd44a
lumzero <j.witwit@student.avans.nl>
2017-04-12 17:26:17 +0200

    Ios/feature/ios login (#52)
    * First init
    
    * IOS UI
    
    * Implemented register function
    
    * Je krijgt nu een terugkoppeling als je hebt geregistreerd
    
    * Get and store login token from API
    
    * - Remember user configurations after successful login
    - Save token
    
    * Fetch bird collection from the API
    
    * Convert data to JSON for UItable
    
    * Created uiTableView with overview birds
    
    * Added callback segue way for once you’re logged in
    
    * no message

---
+++ IOS/BirdTracker/BirdTracker/ApiManager.swift
@@ -0,0 +1,87 @@
+//
+//  ApiManager.swift
+//  BirdTracker
+//
+//  Created by Jamam Witwit on 10/04/2017.
+//  Copyright © 2017 Witwit. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+
+class ApiManager : NSObject {
+    
+    static let sharedInstance = ApiManager()
+    
+    let baseUrl = "https://vogeltracker.herokuapp.com/"
+    
+    func register (user : Dictionary<String, String>, completion: @escaping (_ result: Dictionary<String, Any>?, _ error: Error?) -> Void) {
+        
+        guard let endpoint = URL(string: baseUrl + "register") else {
+                    print("Not a valid url")
+                    return
+        }
+        
+        Alamofire.request(endpoint, method: .post, parameters: user).responseJSON { response in
+            
+            switch response.result {
+            case .success(let value):
+                completion(value as? Dictionary, response.error)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+    }
+    
+    func login (user: Dictionary<String, String>, completion: @escaping (_ result: Dictionary<String, Any>?, _ error: Error?) -> Void) {
+        
+        guard let endpoint = URL(string: baseUrl + "login") else {
+            print("not a valid url")
+            return
+        }
+        
+        Alamofire.request(endpoint, method: .post, parameters: user).responseJSON { response in
+            switch response.result {
+                case .success(let value) :
+                    completion(value as? Dictionary, response.error)
+                case .failure(let error):
+                    completion (nil, error)
+            }
+        }
+    }
+    
+    func getBirds(token : String, completion: @escaping (_ result: JSON, _ error: Error?) -> Void)  {
+        
+        guard let endpoint = URL(string: baseUrl + "birds") else {
+            print("not a valid url")
+            return
+        }
+        
+        let headers : HTTPHeaders = [
+            "Authorization" : "JWT " + token,
+            "Accept": "application/json"
+        ]
+        
+        Alamofire.request(endpoint, headers: headers)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                    //print(response.value)
+                    let json = JSON(response.value)
+                    completion (json, response.error)
+                    //print(json)
+                case .failure(let error):
+                    print(error)
+                }
+        }
+        
+        
+    }
+    
+    
+}
---
+++ IOS/BirdTracker/BirdTracker/AuthService.swift
@@ -0,0 +1,62 @@
+//
+//  AuthService.swift
+//  BirdTracker
+//
+//  Created by Jamam Witwit on 09/04/2017.
+//  Copyright © 2017 Witwit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+public class AuthService : ParentService {
+    
+    private var tokenInfo:OAuthInfo!
+    
+    // We create a struct to hold users OAuth information
+    struct OAuthInfo {
+        let token : String
+    
+        init(token: String){
+           self.token = token
+           UserDefaults.standard.set(self.token, forKey: "token")
+           UserDefaults.standard.set(true, forKey: "isLoggedIn")
+        }
+    }
+    
+    
+    func registerNewUser (newUser : Dictionary<String,String>, completion: @escaping (_ message: String ) -> Void ) {
+
+        manager.register(user: newUser) { (result: Dictionary<String, Any>?, error: Error?) in
+
+            self.message = (result!["message"] as? String)!
+            
+            if(self.message == "ok") {
+               completion("Registratie is voltooid")
+            }
+        }
+        
+
+    }
+    
+    func login (existingUser : Dictionary<String, String>,  completion: @escaping (_ message: String ) -> Void ) {
+        
+        manager.login(user: existingUser){ (result: Dictionary<String, Any>?, error: Error?) in
+            
+            self.message = (result!["message"] as? String)!
+            
+            if(self.message == "ok") {
+                
+                let token : String = (result!["token"] as? String)!
+                OAuthInfo.init(token: token)
+                completion("success")
+            }
+            
+        }
+        
+    }
+    
+    
+
+}
