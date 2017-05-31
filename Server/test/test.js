af127d67bfe4f0e2ad4b1e8eb7ac4cad91d55a0e
Jamam Witwit <jamamwitwit@MacBook-Pro-van-Jamam.local>
2017-05-31 21:42:31 +0200

    Added more tests

--- Server/test/test.js
+++ Server/test/test.js
@@ -78,6 +78,87 @@ describe("Birds", function(){
         .expect(401, done);
     })
 
+    it('Put a single bird should return a 200 response', function(done){
+        server.put('/birds/' + "5917775dffd6a211d345e556")
+        .send({
+            _id : "5917775dffd6a211d345e556",
+            latin_name: "(Great) Cormorant, Phalacrocorax carbo",
+            information: "Een sigaar met vleugels - dat is typisch een aalscholver in vlucht. Het zijn onmiskenbare vogels, mede door de lange snavel met haakpunt. Aalscholvers zijn koloniebroeders. Het menu bestaat uitsluitend uit vis. In tegenstelling tot wat de naam doet vermoeden vormt paling slechts een zeer klein deel van het menu. Uit onderzoek is gebleken dat vooral brasem wordt gegeten, die in groten getale uit het IJsselmeer worden gevist. Commercieel gezien is de brasem niet interessant en aalscholvers vormen dus niet of nauwelijks een bron van concurrentie met de binnenvisserij. Door vermeende concurrentie werden aalscholvers verguisd en afgeschilderd als visstropers.\nIn tegenstelling tot vrijwel alle andere watervogels bevat hun verenkleed slechts zeer weinig vet. Daardoor is het niet waterdicht en wordt een duikende aalscholver drijfnat. Na een duik moet een aalscholver dus drogen. Dit doen ze door met half gespreide vleugels op een paal of in een boom te gaan zitten; een zeer markante houding.",
+            name: "Aalscholver",
+            trend_and_amount: "Sinds 1970 is de aalscholverpopulatie fors toegenomen, na eerder door een diep dal gegaan te zijn. De stand heeft zich sinds de jaren '90 min of meer gestabiliseerd rond de 19.000 tot 20.000 paren. Deze broeden voornamelijk in het IJsselmeergebied, het rivierengebied en in de plassen- en merengebieden." 
+        })
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', authorization)
+        .expect(200, done);
+    })
+
+    it('Put a single bird should return a 401 response when unauthorized', function(done){
+        server.put('/birds/' + "5917775dffd6a211d345e556")
+        .send({
+            _id : "5917775dffd6a211d345e556",
+            latin_name: "(Great) Cormorant, Phalacrocorax carbo",
+            information: "Een sigaar met vleugels - dat is typisch een aalscholver in vlucht. Het zijn onmiskenbare vogels, mede door de lange snavel met haakpunt. Aalscholvers zijn koloniebroeders. Het menu bestaat uitsluitend uit vis. In tegenstelling tot wat de naam doet vermoeden vormt paling slechts een zeer klein deel van het menu. Uit onderzoek is gebleken dat vooral brasem wordt gegeten, die in groten getale uit het IJsselmeer worden gevist. Commercieel gezien is de brasem niet interessant en aalscholvers vormen dus niet of nauwelijks een bron van concurrentie met de binnenvisserij. Door vermeende concurrentie werden aalscholvers verguisd en afgeschilderd als visstropers.\nIn tegenstelling tot vrijwel alle andere watervogels bevat hun verenkleed slechts zeer weinig vet. Daardoor is het niet waterdicht en wordt een duikende aalscholver drijfnat. Na een duik moet een aalscholver dus drogen. Dit doen ze door met half gespreide vleugels op een paal of in een boom te gaan zitten; een zeer markante houding.",
+            name: "Aalscholver",
+            trend_and_amount: "Sinds 1970 is de aalscholverpopulatie fors toegenomen, na eerder door een diep dal gegaan te zijn. De stand heeft zich sinds de jaren '90 min of meer gestabiliseerd rond de 19.000 tot 20.000 paren. Deze broeden voornamelijk in het IJsselmeergebied, het rivierengebied en in de plassen- en merengebieden." 
+        })
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', "")
+        .expect(401, done);
+    })
+
+    it('Put a single bird should return a 400 response without proper id', function(done){
+        server.put('/birds/' + "5917775dffd6a211d345e556")
+        .send({
+            _id : "zz",
+            latin_name: "(Great) Cormorant, Phalacrocorax carbo",
+            information: "Een sigaar met vleugels - dat is typisch een aalscholver in vlucht. Het zijn onmiskenbare vogels, mede door de lange snavel met haakpunt. Aalscholvers zijn koloniebroeders. Het menu bestaat uitsluitend uit vis. In tegenstelling tot wat de naam doet vermoeden vormt paling slechts een zeer klein deel van het menu. Uit onderzoek is gebleken dat vooral brasem wordt gegeten, die in groten getale uit het IJsselmeer worden gevist. Commercieel gezien is de brasem niet interessant en aalscholvers vormen dus niet of nauwelijks een bron van concurrentie met de binnenvisserij. Door vermeende concurrentie werden aalscholvers verguisd en afgeschilderd als visstropers.\nIn tegenstelling tot vrijwel alle andere watervogels bevat hun verenkleed slechts zeer weinig vet. Daardoor is het niet waterdicht en wordt een duikende aalscholver drijfnat. Na een duik moet een aalscholver dus drogen. Dit doen ze door met half gespreide vleugels op een paal of in een boom te gaan zitten; een zeer markante houding.",
+            name: "Aalscholver",
+            trend_and_amount: "Sinds 1970 is de aalscholverpopulatie fors toegenomen, na eerder door een diep dal gegaan te zijn. De stand heeft zich sinds de jaren '90 min of meer gestabiliseerd rond de 19.000 tot 20.000 paren. Deze broeden voornamelijk in het IJsselmeergebied, het rivierengebied en in de plassen- en merengebieden." 
+        })
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', authorization)
+        .expect(400, done);
+    })
+
+    it('Put a single bird without content should return a 204 response', function(done){
+        server.put('/birds/:id')
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', authorization)
+        .expect(204, done);
+    })
+
+    it('Patch a single bird should return a 200 response', function(done){
+        server.patch('/birds/' + "5917775dffd6a211d345e556")
+        .send({
+            _id : "5917775dffd6a211d345e556",
+            latin_name: "(Great) Cormorant, Phalacrocorax carbo"        
+        })
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', authorization)
+        .expect(200, done);
+    })
+
+    it('Patch a single bird should return a 401 response without authorization', function(done){
+        server.patch('/birds/' + "5917775dffd6a211d345e556")
+        .send({
+            _id : "5917775dffd6a211d345e556",
+            latin_name: "(Great) Cormorant, Phalacrocorax carbo"        
+        })
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', "")
+        .expect(401, done);
+    })
+
+    it('Patch a single bird should return a 400 response without content', function(done){
+        server.patch('/birds/' + "5917775dffd6a211d345e556")
+        .send({
+            id: "zz"     
+        })
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', authorization)
+        .expect(400, done);
+    })
+
     it('Delete a single bird should return a 200 respone', function(done){
         server.delete('/birds/')
         .send({id : "58e763a17cd0d10004957c8f" })
